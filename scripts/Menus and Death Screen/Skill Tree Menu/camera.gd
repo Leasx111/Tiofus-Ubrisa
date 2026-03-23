84085956b2fc716c7f51d92dd7deb502fd9d4e0d
leasx111 <leandrofiorini21@gmail.com>
2026-03-23 17:58:38 +0000

    added zoom prototipe

---
+++ scripts/Menus and Death Screen/Skill Tree Menu/camera.gd
@@ -0,0 +1,22 @@
+extends Camera2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+func _input(event: InputEvent) -> void: 
+	
+	if event.is_action("zoom_in") :
+		
+		self.zoom += Vector2(0.1, 0.1)
+	
+	if event.is_action("zoom_out") :
+		
+		self.zoom -= Vector2(0.1, 0.1)
